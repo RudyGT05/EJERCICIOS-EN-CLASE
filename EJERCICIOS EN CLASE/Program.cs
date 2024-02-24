7fcacd25f8f1425e3733d9c1cab71dd242aaaef3
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-24 11:32:32 -0600

    ejercicio 2.1

--- EJERCICIOS EN CLASE/Program.cs
+++ EJERCICIOS EN CLASE/Program.cs
@@ -1,10 +1,6 @@
-﻿Console.WriteLine("Enunciado 3.1");
-//string pass = "jutiapa";
-//string passI;
-//    do
-//{
-//    Console.WriteLine("Ingrese la contraseña: ");
-//    passI = Console.ReadLine().ToLower();
-
-//}while(passI != pass);
-//Console.WriteLine("Contraseña Correcta");
+﻿int x = 0;
+while (x >= 0)
+{
+    Console.WriteLine("Ingresa un numero");
+    x = int.Parse(Console.ReadLine());
+}
