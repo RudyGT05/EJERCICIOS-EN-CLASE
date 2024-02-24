33fe9faf5616c04fd0d10f0d92cabe3d9703686b
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-24 11:32:10 -0600

    ejercicio 1.2

--- EJERCICIOS EN CLASE/Program.cs
+++ EJERCICIOS EN CLASE/Program.cs
@@ -1,13 +1,10 @@
-﻿Console.WriteLine("Enunciado 1");
-for (int n = 1; n <= 5; n++)
-{
-    Console.WriteLine(n);
-}
-Console.WriteLine("Enunciado 2");
-int s = 0;
-for (int d = 1; d <= 10; d++)
-{
-    Console.WriteLine(d);
-    s = s + d;
-}
-Console.WriteLine(s);
+﻿Console.WriteLine("Enunciado 3.1");
+//string pass = "jutiapa";
+//string passI;
+//    do
+//{
+//    Console.WriteLine("Ingrese la contraseña: ");
+//    passI = Console.ReadLine().ToLower();
+
+//}while(passI != pass);
+//Console.WriteLine("Contraseña Correcta");
