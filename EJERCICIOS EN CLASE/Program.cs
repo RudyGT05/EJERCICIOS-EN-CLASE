e196c89b98a23533869c37b063827fe3a6741e6b
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-24 11:31:21 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIOS EN CLASE/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enunciado 1");
+for (int n = 1; n <= 5; n++)
+{
+    Console.WriteLine(n);
+}
+Console.WriteLine("Enunciado 2");
+int s = 0;
+for (int d = 1; d <= 10; d++)
+{
+    Console.WriteLine(d);
+    s = s + d;
+}
+Console.WriteLine(s);
